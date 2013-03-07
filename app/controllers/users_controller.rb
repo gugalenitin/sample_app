f4d0223953df21d26a3ab07ac2fc0648328a21fb
Nitin Narendra Gugale <nitingugale@Nitins-MacBook-Pro.local>
2013-03-07 11:56:47 +0530

    Changed to iMac

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,9 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
-  def new
+	def new
 		@user = User.new
-  end
+	end
 
 	def create
 		@user = User.new(params[:user])
